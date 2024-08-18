c6d6ffc611a79b951328138510ecd18707747f72
MarkoSur <mk3.filippov@gmail.com>
2024-08-18 23:59:35 +0500

    Add files via upload

---
+++ урок 2 (переменные) .py
@@ -0,0 +1,6 @@
+completed_lessons = '12,'
+total_hours_spent = '1.5,'
+course = 'Pyton, '
+time_for_one_task = '0.125'
+print ('Курс: ' + course + 'всего задач:' + completed_lessons + 'затрачено часов:'
+ + total_hours_spent + ' среднее время выполнения ' + time_for_one_task + ' часа')
