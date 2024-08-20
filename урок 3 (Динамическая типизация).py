37b8cc68c585c69c342f644083224c0b8b9e7965
MarkoSur <mk3.filippov@gmail.com>
2024-08-20 22:20:34 +0500

    Add files via upload

---
+++ урок 3 (Динамическая типизация).py
@@ -0,0 +1,10 @@
+from operator import truediv
+
+name = 'Mark'
+print ('name:', name)
+age = 34
+print ('age:', age)
+new_age = 35
+print ('new age:', new_age)
+Is_student = True
+print ('Is student:', True)
